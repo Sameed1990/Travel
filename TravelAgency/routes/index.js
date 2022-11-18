623ca1c1799c0b4d4e7ace80043eb62cb7d566bf
Sameed1990 <111309698+Sameed1990@users.noreply.github.com>
2022-11-18 12:32:33 -0800

    Add files via upload

--- TravelAgency/routes/index.js
+++ TravelAgency/routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about' , (req, res)=>{
+  res.render('about', {title : 'Express'})
+})
+
+router.get('/service' , (req, res)=>{
+  res.render('service', {title : 'Express'})
+})
+
+router.get('/packages' , (req, res)=>{
+  res.render('package', {title : 'Express'})
+})
+router.get('/contact' , (req, res)=>{
+  res.render('contact', {title : 'Express'})
+})
+router.get('/destination' , (req, res)=>{
+  res.render('destination', {title : 'Express'})
+})
+router.get('/guide' , (req, res)=>{
+  res.render('guide', {title : 'Express'})
+})
+router.get('/testimonial' , (req, res)=>{
+  res.render('testimonial', {title : 'Express'})
+})
+
+
+
+
 module.exports = router;
